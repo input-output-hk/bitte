b7e9e043ef634e49a8a51dcabef1ab5844893be4
Michael Fellinger <michael.fellinger@iohk.io>
2020-08-13 10:08:12 +0200

    fix nixos-modules

--- pkgs/nixos-modules.nix
+++ pkgs/nixos-modules.nix
@@ -6,15 +6,10 @@ let
   mapFilterAttrs = sieve: f: attrs: filterAttrs sieve (mapAttrs' f attrs);
   dir = ../modules;
   paths = builtins.readDir dir;
-
-  local = mapFilterAttrs (key: value: value != null) (name: type:
-    nameValuePair (removeSuffix ".nix" name)
-    (if name != "default.nix" && type == "regular" && hasSuffix ".nix" name then
-      (import (dir + "/${name}"))
-    else
-      null)) paths;
-in local // {
-  amazon-image =
-    import (nixpkgs + "/nixos/modules/virtualisation/amazon-image.nix");
-}
+in mapFilterAttrs (key: value: value != null) (name: type:
+  nameValuePair (removeSuffix ".nix" name)
+  (if name != "default.nix" && type == "regular" && hasSuffix ".nix" name then
+    (import (dir + "/${name}"))
+  else
+    null)) paths
 
