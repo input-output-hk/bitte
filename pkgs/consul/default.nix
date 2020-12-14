44d9d88f59925138536f9407005d16c55509c0cd
Michael Fellinger <michael.fellinger@iohk.io>
2020-12-14 13:37:29 +0100

    consul 1.9.0

--- pkgs/consul/default.nix
+++ pkgs/consul/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "consul";
-  version = "1.8.5";
+  version = "1.9.0";
   rev = "v${version}";
 
   # Note: Currently only release tags are supported, because they have the Consul UI
@@ -17,7 +17,7 @@ buildGoModule rec {
     owner = "hashicorp";
     repo = pname;
     inherit rev;
-    sha256 = "sha256-Re3YcltmwstOrewdZbjPnAqxBg4ubQXLhyfBPtb+w7E=";
+    sha256 = "sha256-OOWmr9XnzyrPQAKcIZJ1j7sFUAhG2BL3Bb4aOOSveRk=";
   };
 
   passthru.tests.consul = nixosTests.consul;
@@ -26,7 +26,7 @@ buildGoModule rec {
   # has a split module structure in one repo
   subPackages = [ "." "connect/certgen" ];
 
-  vendorSha256 = "snt6jshsOzHGRnqnhh8U5F1tZFLlsnFqQun0/SyE4/8=";
+  vendorSha256 = "sha256-4Ck3p7a6gUQgFPPaoKzrBbBTkBvAvGV8RN9V//4xhdU=";
   deleteVendor = true;
 
   preBuild = ''
