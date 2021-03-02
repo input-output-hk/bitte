9bdc66d8e2c66c7b878718a3bacf678699e5414b
Michael Fellinger <michael.fellinger@iohk.io>
2021-03-02 15:11:40 +0100

    use my nixpkgs branch for terraform

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url =
       "github:NixOS/nixpkgs?rev=b8c367a7bd05e3a514c2b057c09223c74804a21b";
     # nixpkgs-terraform.url = "github:anandsuresh/nixpkgs/backport";
-    nixpkgs-terraform.url = "path:/home/manveru/github/nixos/nixpkgs";
+    nixpkgs-terraform.url = "github:manveru/nixpkgs/iohk-terraform";
     inclusive.url = "github:input-output-hk/nix-inclusive";
     utils.url = "github:numtide/flake-utils";
     bitte-cli.url = "github:input-output-hk/bitte-cli";
