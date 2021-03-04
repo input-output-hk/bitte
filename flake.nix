dae95b6cc29376f7a4c5b9eafe95b895cc562b30
Shay Bergmann <shaybergmann@gmail.com>
2021-03-04 14:16:11 +0000

    Switch back to following bitte-cli/master

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nixpkgs-terraform.url = "github:manveru/nixpkgs/iohk-terraform";
     inclusive.url = "github:input-output-hk/nix-inclusive";
     utils.url = "github:numtide/flake-utils";
-    bitte-cli.url = "github:input-output-hk/bitte-cli/nur-rost";
+    bitte-cli.url = "github:input-output-hk/bitte-cli";
     nix.url = "github:NixOS/nix?rev=b19aec7eeb8353be6c59b2967a511a5072612d99";
     ops-lib = {
       url = "github:input-output-hk/ops-lib";
