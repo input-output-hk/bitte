62650f76268cc76af61cb4d3b934e2eac6a080af
Timothy DeHerrera <tim.deherrera@iohk.io>
2021-04-07 10:22:44 -0600

    build nomad auto-scaler from source and update it

--- pkgs/nomad-autoscaler.nix
+++ pkgs/nomad-autoscaler.nix
@@ -1,15 +1,16 @@
-{ stdenv, fetchzip }:
-stdenv.mkDerivation rec {
+{ stdenv, fetchFromGitHub, buildGoModule }:
+buildGoModule rec {
   pname = "nomad-autoscaler";
-  version = "0.1.0";
+  version = "0.3.2";
 
-  src = fetchzip {
-    sha256 = "sha256-jmvCJf37TLyY1lXmrEp3VPSBJaMORI+YHGt3y9sIMUo=";
-    url =
-      "https://releases.hashicorp.com/nomad-autoscaler/${version}/nomad-autoscaler_${version}_linux_amd64.zip";
+  src = fetchFromGitHub {
+    owner = "hashicorp";
+    repo = "nomad-autoscaler";
+    rev = "v${version}";
+    sha256 = "sha256-fiVFKv89ZwEetSmZqMZDP33URy0iF/O90vnQMHp7h2g=";
   };
 
-  installPhase = ''
-    install -m0777 -D $src/nomad-autoscaler $out/bin/nomad-autoscaler
-  '';
+  subPackages = [ "." ];
+
+  vendorSha256 = "sha256-hU8aOQMOSSRs1+/2yUinh6w0PjmefpkC3NQtqG3YxCY=";
 }
