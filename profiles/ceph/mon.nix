5f79fc278adfc7e0252057b4ac32d2501c81b135
Michael Fellinger <michael.fellinger@iohk.io>
2021-04-26 12:21:10 +0200

    more mon fixes

--- profiles/ceph/mon.nix
+++ profiles/ceph/mon.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, nodeName, ... }:
 let
   keyFile = "mon.json";
   cfg = config.services.ceph;
@@ -14,31 +14,30 @@ in {
     ceph = {
       mon = {
         enable = true;
-        daemons = [ "monA" ];
+        daemons = [ "monitoring" ];
       };
 
       mgr = {
         enable = true;
-        daemons = [ "monA" ];
-      };
-    };
-
-    networking = {
-      firewall = {
-        allowedTCPPorts = [ 6789 3300 ];
-        allowedTCPPortRanges = [{
-          from = 6800;
-          to = 7300;
-        }];
+        daemons = [ "monitoring" ];
       };
     };
   };
 
-  systemd.services.ceph-mon-setup = let name = "mon-0";
-  in {
+  networking = {
+    firewall = {
+      allowedTCPPorts = [ 6789 3300 ];
+      allowedTCPPortRanges = [{
+        from = 6800;
+        to = 7300;
+      }];
+    };
+  };
+
+  systemd.services.ceph-mon-setup = {
     wantedBy = [ "multi-user.target" ];
-    before = [ "ceph-mon-${name}" ];
-    path = with pkgs; [ systemd ceph gnugrep vault xfsprogs ];
+    before = [ "ceph-mon-${nodeName}" ];
+    path = with pkgs; [ systemd ceph gnugrep xfsprogs ];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
@@ -47,41 +46,41 @@ in {
       ExecStart = pkgs.writeBashChecked "ceph-mon-setup.sh" ''
         set -exuo pipefail
 
-        if [ -s /var/lib/ceph/mgr/ceph-${name}/keyring ]; then
+        if [ -s /var/lib/ceph/mgr/ceph-${nodeName}/keyring ]; then
           exit 0
         fi
 
-        vault kv put kv/bootstrap/ceph/keyring - \
-        < /run/keys/ceph.client.admin.keyring
-
         monmaptool \
+          --clobber \
           --create \
-          --add ${name} ${config.cluster.instances.${name}.privateIP} \
+          --add ${nodeName} ${config.cluster.instances.${nodeName}.privateIP} \
           --fsid ${cfg.global.fsid} \
           /tmp/monmap
 
-        ceph ceph-mon --mkfs \
-          -i ${cfg.monA.name} \
+        ceph-mon --mkfs \
+          -i ${nodeName} \
           --monmap /tmp/monmap \
           --keyring /run/keys/ceph.mon.keyring
 
-        ceph mkdir -p /var/lib/ceph/mgr/ceph-${name}/
-        ceph touch /var/lib/ceph/mon/ceph-${name}/done
+        mkdir -p /var/lib/ceph/mgr/ceph-${nodeName}/
+        touch /var/lib/ceph/mon/ceph-${nodeName}/done
 
-        systemctl start ceph-mon-${name}
+        systemctl start ceph-mon-${nodeName}
         ceph mon enable-msgr2
 
         ceph -s | grep 'mon: 1 daemons'
 
-        ceph auth get-or-create mgr.${name} mon 'allow profile mgr' osd 'allow *' mds 'allow *' \
-        > /var/lib/ceph/mgr/ceph-${name}/keyring
-        systemctl start ceph-mgr-${name}
+        ceph auth get-or-create mgr.${nodeName} mon 'allow profile mgr' osd 'allow *' mds 'allow *' \
+        > /var/lib/ceph/mgr/ceph-${nodeName}/keyring
+        systemctl start ceph-mgr-${nodeName}
       '';
     };
   };
 
   secrets.generate.mon = ''
-    export PATH="${lib.makeBinPath (with pkgs; [ coreutils sops toybox ceph jq ])}"
+    export PATH="${
+      lib.makeBinPath (with pkgs; [ coreutils sops toybox ceph jq ])
+    }"
     target="encrypted/${keyFile}"
 
     if [ ! -s "$target" ]; then
