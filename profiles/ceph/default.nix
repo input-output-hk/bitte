21cc92c0decd86d5f33d63e012c159ada490a7b2
Michael Fellinger <michael.fellinger@iohk.io>
2021-04-26 16:13:14 +0200

    more ceph config

--- profiles/ceph/default.nix
+++ profiles/ceph/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, nodeName, ... }: {
   imports = [ ../common.nix ../telegraf.nix ];
 
   environment.systemPackages = with pkgs; [ ceph xfsprogs netcat-openbsd ];
@@ -9,10 +9,19 @@
     ceph = {
       enable = true;
 
+      extraConfig = {
+        public_addr = config.cluster.instances.${nodeName}.privateIP;
+      };
+
       global = {
         fsid = "7603b881-c1f8-487c-995e-50ac5d2ee0ee";
         monHost = config.cluster.instances.monitoring.privateIP;
         monInitialMembers = "monitoring";
+      };
+
+      rgw = {
+        enable = true;
+        daemons = [nodeName];
       };
     };
   };
