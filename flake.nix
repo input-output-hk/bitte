cc22f117e19739a14597f9332d18df5c120fe0f0
Shay Bergmann <shay.bergmann@iohk.io>
2021-06-08 20:11:35 +0000

    Bump utils to support apps, mode mkHashiStack into lib

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
       "github:NixOS/nixpkgs?rev=b8c367a7bd05e3a514c2b057c09223c74804a21b";
     nixpkgs-terraform.url = "github:input-output-hk/nixpkgs/iohk-terraform-2021-06";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    utils.url = "github:kreisys/flake-utils";
     bitte-cli.url = "github:input-output-hk/bitte-cli";
     hydra-provisioner.url = "github:input-output-hk/hydra-provisioner";
     nix.url = "github:NixOS/nix?rev=b19aec7eeb8353be6c59b2967a511a5072612d99";
@@ -30,37 +30,50 @@
   };
 
   outputs = { self, hydra-provisioner, nixpkgs, utils, bitte-cli, ... }@inputs:
-    let overlay = import ./overlay.nix inputs;
-    in (utils.lib.eachSystem [ "x86_64-linux" ] (system: rec {
+  utils.lib.simpleFlake {
+    inherit nixpkgs;
 
-      legacyPackages = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true; # for ssm-session-manager-plugin
-        overlays = [ overlay hydra-provisioner.overlay ];
-      };
+    systems = [ "x86_64-linux" ];
 
-      inherit (legacyPackages) devShell;
+    preOverlays = [ hydra-provisioner ];
+    overlay = import ./overlay.nix inputs;
+    config.allowUnfree = true; # for ssm-session-manager-plugin
 
-      packages = {
-        inherit (legacyPackages)
-          bitte cfssl consul cue glusterfs haproxy haproxy-auth-request
-          haproxy-cors nixFlakes nixos-rebuild nomad nomad-autoscaler
-          oauth2_proxy sops ssm-agent terraform-with-plugins vault-backend
-          vault-bin;
-      };
+    lib = import ./lib { inherit (nixpkgs) lib; };
 
-      hydraJobs = packages;
+    shell = { devShell }: devShell;
 
-      apps.bitte = utils.lib.mkApp { drv = legacyPackages.bitte; };
+    packages = {
+      bitte
+    , cfssl
+    , consul
+    , cue
+    , glusterfs
+    , haproxy
+    , haproxy-auth-request
+    , haproxy-cors
+    , nixFlakes
+    , nixos-rebuild
+    , nomad
+    , nomad-autoscaler
+    , oauth2_proxy
+    , sops
+    , ssm-agent
+    , terraform-with-plugins
+    , vault-backend
+    , vault-bin
+    }@pkgs: pkgs;
 
-    })) // {
-      lib = import ./lib { inherit (nixpkgs) lib; };
-      inherit overlay;
-      mkHashiStack = import ./lib/mk-hashi-stack.nix;
+    hydraJobs = self.packages;
 
-      nixosModules = let
-        modules = self.lib.mkModules ./modules;
-        default.imports = builtins.attrValues modules;
-      in modules // { inherit default; };
+    apps = { bitte }: {
+      bitte = utils.lib.mkApp { drv = bitte; };
+      defaultApp = utils.lib.mkApp { drv = bitte; };
     };
-  }
+
+    nixosModules = let
+      modules = self.lib.mkModules ./modules;
+      default.imports = builtins.attrValues modules;
+    in modules // { inherit default; };
+  };
+}
--- lib/default.nix
+++ lib/default.nix
@@ -3,5 +3,6 @@
   sanitize = import ./sanitize.nix { inherit lib snakeCase; };
   snakeCase = import ./snake-case.nix { inherit lib; };
   mkModules = import ./make-modules.nix { inherit lib; };
+  mkHashiStack = import ./mk-hashi-stack.nix;
 }
 
