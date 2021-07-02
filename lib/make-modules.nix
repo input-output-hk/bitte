384b13bc15006a21ac4ee76ba8c6243b07a21941
Michael Fellinger <michael.fellinger@iohk.io>
2021-07-02 17:07:12 +0200

    also fix loading of terraform modules

--- lib/make-modules.nix
+++ lib/make-modules.nix
@@ -3,21 +3,20 @@
 let
   join = a: b: if a == "" then b else "${a}-${b}";
 
-  inherit (builtins) readDir mapAttrs attrValues foldl' elemAt typeOf substring stringLength listToAttrs;
+  inherit (builtins) readDir mapAttrs attrValues foldl' elemAt typeOf substring stringLength listToAttrs trace;
 
   convert = prefix: d:
     let
       entries = readDir d;
-      filterNix = lib.filterAttrs (n: v: lib.strings.hasSuffix ".nix" n);
       expanded = mapAttrs (name: type:
-        if type == "regular" then [
+        if (type == "regular") && (lib.strings.hasSuffix ".nix" name) then [
           (join prefix name)
           (d + "/${name}")
         ] else if type == "directory" then [
           (join prefix name)
           (convert (join prefix name) (d + "/${name}"))
         ] else
-          null) (filterNix entries);
+          null) entries;
     in attrValues expanded;
 
   tree = convert "" dir;
