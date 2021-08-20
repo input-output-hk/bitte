0162f9a50adc19cf928cd42b9db60383ac170b7a
John Lotoski <john.lotoski@iohk.io>
2021-08-20 12:59:04 -0500

    Configs nomad clients to use consul TLS for connect

--- profiles/nomad/client.nix
+++ profiles/nomad/client.nix
@@ -22,6 +22,14 @@
     vault.address = "http://127.0.0.1:8200";
   };
 
+  systemd.services.nomad.environment = {
+    CONSUL_HTTP_ADDR = "https://127.0.0.1:8501";
+    CONSUL_HTTP_SSL = "true";
+    CONSUL_CACERT = config.services.consul.caFile;
+    CONSUL_CLIENT_CERT = config.services.consul.certFile;
+    CONSUL_CLIENT_KEY = config.services.consul.keyFile;
+  };
+
   system.extraDependencies = [ pkgs.pkgsStatic.busybox ];
 
   users.extraUsers.nobody.isSystemUser = true;
