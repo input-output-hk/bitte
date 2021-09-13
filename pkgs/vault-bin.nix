bda1187ca028963b3a83c311778d41cece6fd5f7
John Lotoski <john.lotoski@iohk.io>
2021-09-13 11:56:12 -0500

    Vault bump to v1.8.2

--- pkgs/vault-bin.nix
+++ pkgs/vault-bin.nix
@@ -1,21 +1,24 @@
 { stdenv, lib, fetchurl, unzip, makeWrapper, gawk, glibc }:
 
 let
-  version = "1.8.0";
+  version = "1.8.2";
 
+  # Hashes for misc systems and architectures can be obtained with:
+  #   curl -OL https://releases.hashicorp.com/vault/${version}/vault_${version}_${system}_${arch}.zip
+  #   nix-hash --flat --base32 --type sha256 vault_${version}_${system}_${arch}.zip | nix hash to-sri --type sha256 $(cat -)
   sources = let base = "https://releases.hashicorp.com/vault/${version}";
   in {
     x86_64-linux = fetchurl {
       url = "${base}/vault_${version}_linux_amd64.zip";
-      sha256 = "sha256-H+kPDE8xuu2lgENf4t+vCb+Tni+ChkB8K5ZEgIY3Jyo=";
+      sha256 = "sha256-10ck1swivx4cfFGQCbAXaAms9vHCDuVhB94Mq1TNhGM=";
     };
     x86_64-darwin = fetchurl {
       url = "${base}/vault_${version}_darwin_amd64.zip";
-      sha256 = "sha256-ubG4DLoWWm2ujFu/YgfxyU9mnuZP3sXCwh7g+kwGFH8=";
+      sha256 = "sha256-4zmC64YaU5qBIr9nTDmvMJAYJcBgtMgXXL+gbptdS/U=";
     };
     aarch64-linux = fetchurl {
       url = "${base}/vault_${version}_linux_arm64.zip";
-      sha256 = "sha256-rLo+al+F4tNBxnLHETDDZ4NcxE92/bJWZzIibNV8U6o=";
+      sha256 = "sha256-CgNDNQnyxZSgNV3rZycMd/vG1w6ALMUiNYTDtgAp4gI=";
     };
   };
 
