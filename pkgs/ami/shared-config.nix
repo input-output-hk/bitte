223918051fe67027adc0a8dbd47ae71398c19cfd
David Arnold <david.arnold@iohk.io>
2021-11-11 13:37:30 -0500

    imp(ami): #0 venture into efi-amis again
    - This nixpkgs realeas version contains latest fixes known to work
      for core nodes
    
    - This should set the stage to conduct more involved testing with
      client nodes as well.

--- pkgs/ami/shared-config.nix
+++ pkgs/ami/shared-config.nix
@@ -15,8 +15,8 @@ nixpkgs:
         experimental-features = nix-command flakes ca-references
       '';
 
-      # # systemctl kexec can only be used on efi images
-      # ec2.efi = true;
+      # systemctl kexec can only be used on efi images
+      ec2.efi = true;
       amazonImage.sizeMB = 4096;
 
       environment.systemPackages = [ pkgs.git ];
