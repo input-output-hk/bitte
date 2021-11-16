a7fc4b8fc010983d17b3657eaaa72800210f74d7
David Arnold <david.arnold@iohk.io>
2021-11-16 16:46:08 -0500

    fix(bootstrap): #0 sign vault pki intermediate as part of hydration
    - This command was previously implemented in bitte cli and got removed
    - Now, we do it as part of the cluster hydration until further notice

--- modules/terraform/hydrate.nix
+++ modules/terraform/hydrate.nix
@@ -32,6 +32,89 @@ in {
 
     # hydrate vault backends
 
+    /*
+    Bootstrap vault intermediate pki endpoint
+    with local root CA from well-known locations at
+      - ./secrets/ca.pem
+      - ./secrets/ca-key.pem
+    */
+    resource = {
+      # TODO: commented parts are currently accomplished by a systemd one-shot
+      # vault_pki_secret_backend.pki = {
+      #   description = "Cluster wide TLS/SSL PKI backend";
+      #   path = "pki";
+      # };
+      # vault_pki_secret_backend_config_urls.config_urls = {
+      #   backend = var "vault_pki_secret_backend.pki.path";
+      #   issuing_certificates = [
+      #     "https://vault.${domain}:8200/v1/pki/ca"
+      #   ];
+      #   crl_distribution_points = [
+      #     "https://vault.${domain}:8200/v1/pki/crl"
+      #   ];
+      # };
+      # vault_pki_secret_backend_role.server = {
+      #   backend = var "vault_pki_secret_backend.pki.path";
+      #   name = "server";
+      #       key_type = "ec";
+      #       key_bits = 256;
+      #       allow_any_name = true;
+      #       enforce_hostnames = false;
+      #       generate_lease = true;
+      #       max_ttl = "72h";
+      # };
+      # vault_pki_secret_backend_role.client = {
+      #   backend = var "vault_pki_secret_backend.pki.path";
+      #   name = "client";
+      #       key_type = "ec";
+      #       key_bits = 256;
+      #       allowed_domains = service.consul,${region}.consul;
+      #       allow_subdomains = true;
+      #       generate_lease = true;
+      #       max_ttl = "223h";
+      # };
+      # vault_pki_secret_backend_role.admin = {
+      #   backend = var "vault_pki_secret_backend.pki.path";
+      #   name = "admin";
+      #       key_type = "ec";
+      #       key_bits = 256;
+      #       allow_any_name = true;
+      #       enforce_hostnames = false;
+      #       generate_lease = true;
+      #       max_ttl = "12h";
+      # };
+      vault_pki_secret_backend_intermediate_cert_request.issuing_ca = {
+        # depends_on = [ (id "vault_pki_secret_backend.pki") ];
+        # backend = var "vault_pki_secret_backend.pki.path";
+        backend = "pki";
+        type = "internal";
+        common_name = "vault.${config.cluster.domain}";
+      };
+      tls_locally_signed_cert.issuing_ca = {
+        cert_request_pem = var "vault_pki_secret_backend_intermediate_cert_request.issuing_ca.csr";
+        ca_key_algorithm = "ECDSA";
+        ca_private_key_pem = var "file(\"./secrets/ca-key.pem\")";
+        ca_cert_pem = var "file(\"./secrets/ca.pem\")";
+
+        validity_period_hours = 43800;
+        is_ca_certificate = true;
+        allowed_uses = [
+          "signing"
+          "key encipherment"
+          "cert sign"
+          "crl sign"
+        ];
+      };
+      vault_pki_secret_backend_intermediate_set_signed.issuing_ca = {
+        # backend = var "vault_pki_secret_backend.pki.path";
+        backend = "pki";
+        certificate = ''
+          ${var "tls_locally_signed_cert.issuing_ca.cert_pem"}
+          ${var "file(\"./secrets/ca.pem\")"}
+        '';
+      };
+    };
+
     resource.vault_github_auth_backend.employee = {
       organization = "input-output-hk";
       path = "github-employees";
