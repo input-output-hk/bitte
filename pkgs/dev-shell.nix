1d38bc482ca4140e75a310dd9e874291f2af55c8
David Arnold <david.arnold@iohk.io>
2021-11-17 18:01:40 -0500

    imp(fmt): #0 add treefmt config
    - Use the same as in bitte-cli
    - In case you're looking to make treefmt.toml a lib item: it is repo-
      local for good reason :) - transparency, treefmt bin' config discovery

--- pkgs/dev-shell.nix
+++ pkgs/dev-shell.nix
@@ -14,12 +14,15 @@ mkShell.override { stdenv = stdenvNoCC; } {
     dnsutils
     ipcalc
     jq
-    nixfmt
+    nixpkgs-fmt
+    nodePackages.prettier
     nomad
     openssl
+    shfmt
     sops
     ssm-session-manager-plugin
     terraform-with-plugins
+    treefmt
     vault-bin
   ];
 }
