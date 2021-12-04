d94d456b18d2abf5611be5ebeb19570ada875236
Timothy DeHerrera <tim.deherrera@iohk.io>
2021-12-03 23:32:35 -0700

    deploy: don't prompt for host verification
    Will still fail if host key changes unexpectedly, so should still be
    safe enough to avoid man in the middle attacks, while allow unattended
    deploys.

--- lib/mk-deploy.nix
+++ lib/mk-deploy.nix
@@ -11,16 +11,19 @@ let
 
   deploy' = {
     sshUser = "root";
-    sshOpts = [ "-C" "-i" "${deploySshKey}" ];
-    nodes = builtins.mapAttrs (k: _: {
-      profiles.system.user = "root";
-      profiles.system.path =
-        let system = self.nixosConfigurations.${k}.pkgs.system;
-        in deploy.lib.${system}.activate.nixos self.nixosConfigurations.${k};
-    }) self.nixosConfigurations;
+    sshOpts = [ "-C" "-i" "${deploySshKey}" "-o" "StrictHostKeyChecking=no" ];
+    nodes = builtins.mapAttrs
+      (k: _: {
+        profiles.system.user = "root";
+        profiles.system.path =
+          let system = self.nixosConfigurations.${k}.pkgs.system;
+          in deploy.lib.${system}.activate.nixos self.nixosConfigurations.${k};
+      })
+      self.nixosConfigurations;
   };
 
-in {
+in
+{
   deploy = deploy';
 
   checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks deploy')
