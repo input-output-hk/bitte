c35d86030441d2546f0db80e448d2e8fd616daca
David Arnold <david.arnold@iohk.io>
2021-12-03 19:15:05 -0500

    imp: swap consul for raft vault backend
    - The majority of clusters are already migrated

--- profiles/vault/server.nix
+++ profiles/vault/server.nix
@@ -2,8 +2,6 @@
 let
   inherit (config.cluster) instances;
   inherit (instances.${nodeName}) privateIP;
-
-  owendKey = "/var/lib/vault/cert-key.pem";
 in
 {
   imports = [ ./default.nix ./policies.nix ];
@@ -17,11 +15,18 @@ in
 
       listener.tcp = { clusterAddress = "${privateIP}:8201"; };
 
-      storage.consul = lib.mkDefault {
-        address = "127.0.0.1:8500";
-        tlsCaFile = pkiFiles.caCertFile;
-        tlsCertFile = pkiFiles.certChainFile;
-        tlsKeyFile = owendKey;
+      storage.raft = let
+        vcfg = config.services.vault.listener.tcp;
+        instances = lib.filterAttrs
+          (k: v: lib.hasPrefix "core-" k)
+          config.cluster.instances;
+      in lib.mkDefault {
+        retryJoin = lib.mapAttrsToList (_: v: {
+          leaderApiAddr = "https://${v.privateIP}:8200";
+          leaderCaCertFile = vcfg.tlsClientCaFile;
+          leaderClientCertFile = vcfg.tlsCertFile;
+          leaderClientKeyFile = vcfg.tlsKeyFile;
+        }) instances;
       };
     };
 
