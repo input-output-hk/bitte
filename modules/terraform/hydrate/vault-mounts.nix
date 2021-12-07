72316528495ab3e61027c619b4bf8ac042ab3bf3
David Arnold <david.arnold@iohk.io>
2021-12-07 02:16:05 -0500

    imp(secret-mounts): add runtime & starttime secrets mounts
    - remove god-level access to `kv/` for developers
    - `kv/` hold boostrapping tokens etc.

---
+++ modules/terraform/hydrate/vault-mounts.nix
@@ -0,0 +1,29 @@
+/* Vault Mounts
+   Mounts that are not _already_ necessary for bootstrapping.
+*/
+{ terralib, config, ... }:
+let
+
+  inherit (terralib) var id;
+
+  runtimeSecretsPath = "runtime";
+  starttimeSecretsPath = "starttime";
+
+  __fromTOML = builtins.fromTOML;
+
+in {
+  tf.hydrate.configuration = {
+
+    resource.vault_mount.${runtimeSecretsPath} = {
+      path = "${runtimeSecretsPath}";
+      type = "kv-v2";
+      description = "Applications can (temporarily) access runtime secrets if they have access credentials for them";
+    };
+    resource.vault_mount.${starttimeSecretsPath} = {
+      path = "${starttimeSecretsPath}";
+      type = "kv-v2";
+      description = "Nomad can access starttime secrets via its nomad-cluster role and pass these secrets via env variables";
+    };
+
+  };
+}
