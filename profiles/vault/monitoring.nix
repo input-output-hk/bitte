81baed62e459383f1806baca98ba16cbcf14d0b3
David Arnold <david.arnold@iohk.io>
2022-01-08 14:58:59 -0500

    ref: move vault-agent-monitoring module to profiles
    - specializations should be rather flat then nested to ease reasoning
      about them
    
    
      REFACTORING: proven to have no effect via diff-against-bitte-commit

--- profiles/vault/monitoring.nix
+++ profiles/vault/monitoring.nix
@@ -3,11 +3,22 @@
   Imports = { imports = [ ]; };
 
   Switches = {
-    services.vault-agent-monitoring.enable = true;
+    services.vault-agent-core.enable = true;
+    services.vault.enable = lib.mkForce false;
   };
 
   Config = {
+    services.vault-agent-core = {
+      vaultAddress = "https://core.vault.service.consul:8200";
+    };
     services.vault-agent = {
+
+      listener = [{
+        type = "tcp";
+        address = "127.0.0.1:8200";
+        tlsDisable = true;
+      }];
+
       templates = let
         command =
           "${pkgs.systemd}/bin/systemctl try-restart --no-block ingress.service";
@@ -41,6 +52,8 @@
         };
       };
     };
+
+    environment.variables.VAULT_ADDR = "http://127.0.0.1:8200";
   };
 
 in Imports // lib.mkMerge [
--- profiles/vault/routing.nix
+++ profiles/vault/routing.nix
@@ -3,10 +3,25 @@
   Imports = { imports = [ ]; };
 
   Switches = {
-    services.vault-agent-monitoring.enable = true;
+    services.vault-agent-core.enable = true;
+    services.vault.enable = lib.mkForce false;
   };
 
-  Config = { };
+  Config = {
+    services.vault-agent-core = {
+      vaultAddress = "https://core.vault.service.consul:8200";
+    };
+    services.vault-agent = {
+
+      listener = [{
+        type = "tcp";
+        address = "127.0.0.1:8200";
+        tlsDisable = true;
+      }];
+    };
+
+    environment.variables.VAULT_ADDR = "http://127.0.0.1:8200";
+  };
 
 in Imports // lib.mkMerge [
   Switches
