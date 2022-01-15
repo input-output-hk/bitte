6c40616bd7ce9128280faee5078f130e86979331
John Lotoski <john.lotoski@iohk.io>
2022-01-14 22:41:27 -0600

    imp: adds prem based cert pkiFiles

--- lib/mk-system/constants-module.nix
+++ lib/mk-system/constants-module.nix
@@ -1,16 +1,31 @@
-{ config, ... }: {
+{ config, ... }:
+let
+  deployType = config.currentCoreNode.deployType or config.currentAwsAutoScalingGroup.deployType;
+  domain = config.${if deployType == "aws" then "cluster" else "currentCoreNode"}.domain;
+in {
   _module.args = {
     pkiFiles = {
+      # Common deployType cert files
       caCertFile = "/etc/ssl/certs/ca.pem";
+
+      # "aws" deployType cert files
       certChainFile = "/etc/ssl/certs/full.pem";
       certFile = "/etc/ssl/certs/cert.pem";
       keyFile = "/etc/ssl/certs/cert-key.pem";
+
+      # "prem" and "premSim" deployType cert files
+      clientCertFile = "/etc/ssl/certs/client.pem";
+      clientKeyFile = "/etc/ssl/certs/client-key.pem";
+      clientCertChainFile = "/etc/ssl/certs/client-full.pem";
+      serverCertFile = "/etc/ssl/certs/server.pem";
+      serverKeyFile = "/etc/ssl/certs/server-key.pem";
+      serverCertChainFile = "/etc/ssl/certs/server-full.pem";
     };
 
     letsencryptCertMaterial = {
-      certFile = "/etc/ssl/certs/${config.cluster.domain}-cert.pem";
-      certChainFile = "/etc/ssl/certs/${config.cluster.domain}-full.pem";
-      keyFile = "/etc/ssl/certs/${config.cluster.domain}-key.pem";
+      certFile = "/etc/ssl/certs/${domain}-cert.pem";
+      certChainFile = "/etc/ssl/certs/${domain}-full.pem";
+      keyFile = "/etc/ssl/certs/${domain}-key.pem";
     };
 
     gossipEncryptionMaterial = {
