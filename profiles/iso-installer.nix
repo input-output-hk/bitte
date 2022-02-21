cea7fb17a7f3e9e68c9392f21b0d68836ce5aaa6
John Lotoski <john.lotoski@iohk.io>
2022-02-20 19:06:26 -0600

    imp: add iso-installer profile

---
+++ profiles/iso-installer.nix
@@ -0,0 +1,52 @@
+{ lib, pkgs, ... }: {
+  imports = [ ./auxiliaries/nix.nix ./auxiliaries/ssh.nix ];
+
+  disabledModules = [ "virtualisation/amazon-image.nix" ];
+
+  environment = {
+    systemPackages = with pkgs; [
+      bat
+      bind
+      cfssl
+      di
+      fd
+      file
+      gitMinimal
+      htop
+      iptables
+      jq
+      (lib.lowPrio inetutils)
+      lsof
+      ncdu
+      nettools
+      openssl
+      ripgrep
+      sops
+      tcpdump
+      tmux
+      tree
+      vim
+    ];
+  };
+
+  documentation.enable = true;
+  documentation.nixos.enable = true;
+  networking.firewall.allowPing = true;
+  boot.cleanTmpDir = true;
+  time.timeZone = "UTC";
+
+  services.chrony.enable = true;
+  networking.hostName = "iso-installer";
+
+  users.extraUsers = {
+    root.initialHashedPassword = lib.mkForce null;
+    nixos.initialHashedPassword = lib.mkForce null;
+  };
+
+  services.getty.helpLine = lib.mkForce ''
+    Welcome to the bitte nixOS installer.
+    1) Verify network connectivity (ex: ping 8.8.8.8)
+    2) Follow instructions provided by devOps to proceed
+    3) CAUTION: Running any install scripts will WIPE storage drives!
+  '';
+}
