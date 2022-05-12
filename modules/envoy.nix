0ebc83fdf5f21d4f609c8c61050cffb26b86112b
Shay Bergmann <shay.bergmann@iohk.io>
2022-05-12 19:07:52 +0000

    Disable upstream envoy module

--- modules/envoy.nix
+++ modules/envoy.nix
@@ -1,6 +1,7 @@
 { lib, config, ... }:
 let cfg = config.services.envoy;
 in {
+  disabledModules = [ "services/networking/envoy.nix" ];
   options = { services.envoy.enable = lib.mkEnableOption "Enable Envoy"; };
 
   config = { systemd.services.envoy = lib.mkIf cfg.enable { }; };
