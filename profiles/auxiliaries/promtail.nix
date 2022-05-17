2f3e9b21085dbaa7b5da71b29e96d4c75e162614
Shay Bergmann <shay.bergmann@iohk.io>
2022-05-17 16:48:37 +0000

    promtail: only enable promtail if monitoring exists

--- profiles/auxiliaries/promtail.nix
+++ profiles/auxiliaries/promtail.nix
@@ -1,8 +1,10 @@
-_: {
-  services.promtail.enable = true;
+{ lib, config, ... }: {
+  services.promtail = lib.mkIf (config.cluster.nodes ? monitoring) {
+    enable = true;
 
-  services.promtail.clients = [{
-    url =
-      "http://${config.cluster.nodes.monitoring.privateIP}:3100/loki/api/v1/push";
-  }];
+    clients = [{
+      url =
+        "http://${config.cluster.nodes.monitoring.privateIP}:3100/loki/api/v1/push";
+    }];
+  };
 }
